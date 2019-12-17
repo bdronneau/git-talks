16ab826b8aecb046433655e481cf4126b9516fe5
bdronneau <basti1.dr@gmail.com>
2019-12-17 14:22:07 +0100

    WIP

--- app/server.go
+++ app/server.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"net/http"
-	
+	"fmt"
+
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+
 	e.GET("/", func(c echo.Context) error {
+		fmt.Println("I'm alive in log !")
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/users/:id", getUser)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
 }
